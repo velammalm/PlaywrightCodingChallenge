66b3f0ba1155d05582da326cbd57927ca955644a
velammalm <33392369+velammalm@users.noreply.github.com>
2024-11-01 11:17:17 +0100

    changes in playwright.config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://www.ab-in-den-urlaub.ch/',
+    baseURL: 'https://www.ab-in-den-urlaub.de/',
     headless: false,
     launchOptions: {
       // 1
@@ -72,7 +72,7 @@ export default defineConfig({
       use: { 
        // ...devices['Desktop Safari'] 
        browserName: 'webkit',
-        viewport: null,
+        viewport: { width: 1600, height: 1000 },
       },
     },
 
