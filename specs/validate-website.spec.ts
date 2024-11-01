27612d82153047ef05bc2e8d9ac2bec07562d3b1
velammalm <33392369+velammalm@users.noreply.github.com>
2024-11-01 11:44:54 +0100

    Updated Spec File with detailed comments

--- specs/validate-website.spec.ts
+++ specs/validate-website.spec.ts
@@ -24,13 +24,15 @@ test.describe("Validate the website ab in den Urlaub", () => {
     const base_URL = test.info().project.use.baseURL;
     const homePage = new HomePage();
 
-    await homePage.navigateToHomePage(base_URL as String);
-    await homePage.acceptCookies();
+    await homePage.navigateToHomePage(base_URL as String); /* Launching the website */
+    await homePage.acceptCookies(); /* Accepting the cookies */
 
+    /* Validate whether the basic search fields such Destination selction box, travelPeriod date picker, No.of travellers, destination fields */
     await homePage.validateBasicSearchFieldsInHomePage();
 
     const sharedHeader = new SharedHeader();
 
+    /* Validate whether shared header with MyAccount menu is displayed  */
     sharedHeader.verifyMyAccountMenuIsDisplayed();
   });
 
@@ -39,29 +41,38 @@ test.describe("Validate the website ab in den Urlaub", () => {
     const base_URL = test.info().project.use.baseURL;
     const homePage = new HomePage();
 
-    await homePage.navigateToHomePage(base_URL as String);
-    await homePage.acceptCookies();
+    await homePage.navigateToHomePage(base_URL as String); /* Launching the website */
+    await homePage.acceptCookies(); /* Accepting the cookies */
 
     const sharedHeader = new SharedHeader();
-    await sharedHeader.clickAnmeldenFromMyAccount();
+    await sharedHeader.clickAnmeldenFromMyAccount(); /* Click Anmelden from My Account Menu in the shared header */
 
     const login = new Login();
+
+    /* Verify whether the Sign In Widget appears */
     await login.verifySignInWidgetAppears();
+
+    /* Verify that the SignIn Button is disabled initially when user doesn't input both username and password */
     await login.verifySignInButtonDisabled();
 
+    /*enter the credentials which is taken from the data.json under 'test-data' folder*/
     await login.enterCredentials(userName,password);
 
+    /*Verify SignIn button is enabled when user clicks on the SignIn Button*/
     if (await login.verifySignInButtonEnabled()) {
       await login.clickSignInButton();
     }
 
+    /*Verify the error alert text is displayed when the user enters incorrect username and password*/
     await login.verifyErrorAlert();
 
+    /*Verify whether the Email and Password text box border is highlighter in red colour and error texts are displayed under the email &
+    password textbox*/
     await login.verify_RedBorder_HighLight_AndErroText_Email_Password();
   });
 
 
-  test.only("Validation of offers", async () => {
+  test("Validation of offers", async () => {
     /* Setting test timeout to 3 minutes */
     test.setTimeout(TEST_TIMEOUT);
 
@@ -77,20 +88,26 @@ test.describe("Validate the website ab in den Urlaub", () => {
     await homePage.clickOfferSearchButton(); //click on submit button in the homepage
     
     const searchResultPage = new SearchResults();
-    await searchResultPage.validateSearchResultsPage();
-    await searchResultPage.clickLoadMoreButton();
-    const { hotelName, hotelLocation }=await searchResultPage.selectHotel();
+    await searchResultPage.validateSearchResultsPage(); //Validate whether the navigation to SearchRersultsPage is successful
+    await searchResultPage.clickLoadMoreButton(); //In the Search Result page scroll to bottom of the page and click on Load More button
+    const { hotelName, hotelLocation }=await searchResultPage.selectHotel(); //Select the hotel from the newly loaded results and retrieve the hotel name and location details
 
     await switchPage(2);
     const offersPage = new OffersPage();
-    await offersPage.validateOfferSelectionPage();
+    await offersPage.validateOfferSelectionPage(); //Validate whether the navigation to Offer Selection Page is successful
+
+    /*Verify the hotel Name and location details is exactly the same as displayed in previous search result page*/
     await offersPage.verifyHotelDisplayDetails(hotelName, hotelLocation);
+
+    /*Click on the offer and retrieve the price details*/
     const price=await offersPage.selectOffer();
    
 
     await switchPage(3)
     const bookingPage = new BookingPage();
-    await bookingPage.validateBookingPage();
+    await bookingPage.validateBookingPage(); //Validate whether the navigation to Booking Page is successful
+
+    /*Verify whether the hotel price is exactly the same as displayed in previous offer selection page*/
     await bookingPage.validateBookingPrice(price);  
   });
 
